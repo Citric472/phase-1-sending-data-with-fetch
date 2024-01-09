ae40afd0249caa14434773d774f201a0f5d1387c
=Memo <=chepkemoicynthia4@gmail.com>
2024-01-09 21:43:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch('http://localhost:3000/users', configObject)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        document.body.innerHTML += data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+      
+  }
+  
+  
+  
